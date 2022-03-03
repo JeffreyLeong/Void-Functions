f3424f5417a6e329626e8fd376e235d82ea640ca
dollarortwo <jleong001@gmail.com>
2022-03-02 22:12:09 -0800

    added function declaration and void function

---
+++ Temperature-Converter.cpp
@@ -0,0 +1,18 @@
+// Program to convert a Fahrenheit temperature to a Celsius temperature.
+
+#include<iostream>
+using namespace std;
+
+void initialize_screen();
+// Separates current output from the output of the previously run program.
+
+double celsius(double fahrenheit);
+// Converts a Fahrenheit temperature to a Celsius temperature.
+
+void show_results(double f_degrees, double c_degrees);
+// Displays output. Assumes that c_degrees Celsius is equivalent to f_degrees Fahrenheit.
+
+int main()
+{
+ return 0;
+}
